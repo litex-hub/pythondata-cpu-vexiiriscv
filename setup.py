15cfab529a17c473d0fc75edf3f409eb374cef35
Florent Kermarrec <florent@enjoy-digital.fr>
2026-05-26 16:49:34 +0200

    packaging: fix release installs

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from pythondata_cpu_vexiiriscv import version_str
+with open("pythondata_cpu_vexiiriscv/__init__.py", "r") as fh:
+    version_str = re.search(
+        r'^version_str = "([^"]+)"',
+        fh.read(),
+        re.MULTILINE,
+    ).group(1)
 
 setuptools.setup(
     name="pythondata-cpu-vexiiriscv",
@@ -24,7 +30,7 @@ Python module containing verilog files for VexiiRiscv cpu.""",
     zip_safe=False,
     packages=setuptools.find_packages(),
     package_data={
-    	'cpu_vexiiriscv': ['cpu_vexiiriscv/verilog/**'],
+    	'pythondata_cpu_vexiiriscv': ['verilog/**'],
     },
     include_package_data=True,
     project_urls={
